37fa98413479885d7609b8d55526177019619295
Moroz Roman <morozdef24@gmail.com>
2025-08-29 23:18:03 +0300

    finish L1.2

---
+++ L1.2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func squareArrElements(arr []int, id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Gorutine %d start\n", id)
+
+	for idx, el := range arr {
+		arr[idx] = el * el
+	}
+	
+	fmt.Printf("Gorutine %d finish\n", id)
+}
+
+func main() {
+	arr1 := []int{2, 4, 6, 8, 10}
+	arr2 := []int{2, 4, 6, 8, 10}
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
+	go squareArrElements(arr1, 1, &wg)
+	go squareArrElements(arr2, 2, &wg)
+	
+	wg.Wait()
+
+	fmt.Println(arr1)
+	fmt.Println(arr2)
+}
